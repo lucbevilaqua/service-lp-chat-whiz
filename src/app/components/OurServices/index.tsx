1c854dda505abe65f67b360c5531ebcfb19ab537
lucas bevilaqua araujo de oliveira <laobevilaqua@outlook.com>
2025-06-07 19:55:58 +0100

    refactor(services): add a11y and class names

--- src/app/components/OurServices/index.tsx
+++ src/app/components/OurServices/index.tsx
@@ -2,69 +2,66 @@ import Image from "next/image";
 
 export default function OurServices() {
     return (
-        <section className="row">
-            <h2 style={{ marginBottom: 16, fontSize: 48, display: 'flex', alignItems: 'center', justifyContent: 'center', height: 200 }}>Our Services</h2>
-            <div className="col">
+        <section className="row mt-5" id="services" aria-labelledby="our-services-title">
+            <h2
+                id="our-services-title"
+                className="display-4 mb-4 text-center fw-bold"
+            >
+                Our Services
+            </h2>
+            <div className="col-12 col-md">
                 <Image
-                    src="/image-6.png"
-                    alt=""
+                    src="/images/laptop-with-flutter-bot.svg"
+                    alt="Illustration of a chatbot"
                     width={300}
                     height={300}
-                    className="img-fluid"
-                    style={{ borderRadius: 16, marginBottom: 16 }}
+                    style={{ objectFit: "cover" }}
+                    className="rounded mb-3"
                 />
-                <h4>
-                    Creating smart bots
-                </h4>
-                <p>
+                <h3>Creating smart bots</h3>
+                <p className="mt-6 lead ff-raleway">
                     We develop customized bots for WhatsApp and other applications, capable of serving your customers quickly and efficiently, 24/7.
                 </p>
             </div>
-            <div className="col">
+            <div className="col-12 col-md">
                 <Image
-                    src="/image-2.png"
-                    alt=""
+                    src="/images/cellphone-with-flutter-chat.svg"
+                    alt="System integration illustration"
                     width={300}
                     height={300}
-                    className="img-fluid"
-                    style={{ borderRadius: 16, marginBottom: 16 }}
+                    style={{ objectFit: "cover" }}
+                    className="rounded mb-3"
                 />
-                <h4>
-                    Integration with systems
-                </h4>
-                <p>
+                <h3>Integration with systems</h3>
+                <p className="lead ff-raleway">
                     We connect your bots to your company's internal systems, such as CRMs and ERPs, ensuring a fluid and automated experience.
                 </p>
             </div>
-            <div className="col">
+            <div className="col-12 col-md">
                 <Image
-                    src="/image-3.png"
-                    alt=""
+                    src="/images/laptop-with-chat.svg"
+                    alt="Custom conversation flow illustration"
                     width={300}
                     height={300}
-                    className="img-fluid"
-                    style={{ borderRadius: 16, marginBottom: 16 }}
+                    style={{ objectFit: "cover" }}
+                    className="rounded mb-3"
                 />
-                <h4>
-                    Custom conversation flow
-                </h4>
-                <p>
+                <h3>Custom conversation flow</h3>
+                <p className="lead ff-raleway">
                     We create strategic conversation flows, optimized to engage your customers and increase conversions.
                 </p>
             </div>
-            <div className="col">
+            <div className="col-12 col-md">
                 <Image
-                    src="/image-4.png"
-                    alt=""
+                    src="/images/laptop-with-flutter-chat.svg"
+                    alt="Monitoring and support illustration"
                     width={300}
                     height={300}
-                    className="img-fluid"
-                    style={{ borderRadius: 16, marginBottom: 16 }}
+                    style={{ objectFit: "cover" }}
+                    className="rounded mb-3"
                 />
-                <h4>
-                    Continuous monitoring and support
-                </h4>
-                <p>
+                <h3>Continuous monitoring and support</h3>
+                <p className="lead ff-raleway">
                     We provide ongoing support and monitoring to ensure that your bots are always running smoothly.
                 </p>
             </div>
--- src/app/components/Preview/index.tsx
+++ src/app/components/Preview/index.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export default function Preview() {
   return (
-    <section className="row" id="home" aria-labelledby="preview-title">
+    <section className="row mt-5" id="home" aria-labelledby="preview-title">
       <div className="col-12 col-md-4 align-self-start dvh-50 d-flex flex-column justify-content-center">
         <h2 className="text-break-word" id="preview-title">Automating connections, driving results</h2>
         <p className="text-break-word lead ff-raleway">
@@ -19,7 +19,7 @@ export default function Preview() {
           src="/images/laptop-with-chat.svg"
           alt="Laptop displaying a chat interface"
           fill
-          className="rounded my-image"
+          className="rounded"
           style={{ objectFit: "contain" }}
         />
       </div>
