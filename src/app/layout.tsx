3d581890255744f002a116750698982997fa98de
lucas bevilaqua araujo de oliveira <laobevilaqua@outlook.com>
2025-06-07 19:28:13 +0100

    feat: create menu and about sections

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,49 @@
 import type { Metadata } from "next";
-import { Space_Grotesk } from "next/font/google";
-import 'bootstrap/dist/css/bootstrap.css'
+import { Space_Grotesk, Raleway } from "next/font/google";
+import 'bootstrap/dist/css/bootstrap.css';
+import './styles/index.scss';
 import "./globals.css";
-import Head from "next/head";
 
 const spaceGrotesk = Space_Grotesk({
   variable: "--font-space-grotesk",
   subsets: ["latin"],
 });
 
+const raleway = Raleway({
+  variable: "--font-raleway",
+  subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
   title: "Chat Whiz",
-  description: "Automation, bots, digital transformation, Chat Whiz",
+  description: "Automating connections, driving results",
+  keywords: [
+    "automation",
+    "bots",
+    "digital transformation",
+    "Chat Whiz",
+    "AI",
+    "chatbot"
+  ],
+  openGraph: {
+    title: "Chat Whiz",
+    description: "Automating connections, driving results",
+    url: "https://chat-whiz.com",
+    siteName: "Chat Whiz",
+    images: [
+      {
+        url: "/icons/logo.ico",
+        width: 800,
+        height: 600,
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  robots: "index, follow",
+  icons: {
+    icon: "/icons/logo.ico",
+  },
 };
 
 export default function RootLayout({
@@ -21,17 +52,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <Head>
-        <title>Chat Whiz</title>
-        <meta name="description" content="Automating connections, driving results" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta charSet="UTF-8" />
-        <meta name="robots" content="index, follow" />
-        <meta name="keywords" content="automation, bots, digital transformation, Chat Whiz" />
-        <link rel="icon" href="/logo.ico" />
-      </Head>
-      <body className={`${spaceGrotesk.variable}`}>
+    <html lang="en" suppressHydrationWarning>
+      <body className={`${spaceGrotesk.variable} ${raleway.variable}`}>
         {children}
       </body>
     </html>
