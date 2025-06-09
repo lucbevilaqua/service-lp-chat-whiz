4a1614e21ab8c33967a23bff95a320997c6345d1
lucas bevilaqua araujo de oliveira <laobevilaqua@outlook.com>
2025-06-09 13:50:00 +0100

    fix: generate sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,17 +2,14 @@ module.exports = {
   siteUrl: "https://www.chat-whiz.com",
   generateRobotsTxt: true,
   sitemapSize: 7000,
-  changefreq: "weekly",
-  priority: 1,
+  changefreq: "daily",
+  priority: 0.9,
   robotsTxtOptions: {
     policies: [
       {
         userAgent: "*",
         allow: "/",
       },
-    ],
-    additionalSitemaps: [
-      "https://www.chat-whiz.com/sitemap.xml",
-    ],
+    ]
   },
 };
